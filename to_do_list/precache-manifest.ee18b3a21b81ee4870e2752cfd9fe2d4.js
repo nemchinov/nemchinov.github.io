62708c7af104436930cc1a057158226398816b80
A Nemchinov <a.nemchinov@socialquantum.com>
2018-11-14 09:15:46 +0300

    add todo project

---
+++ to_do_list/precache-manifest.ee18b3a21b81ee4870e2752cfd9fe2d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5622afb9e3b78ee9faa8",
+    "url": "/static/js/main.5622afb9.chunk.js"
+  },
+  {
+    "revision": "0195ec8aaa44c3cd8c28",
+    "url": "/static/js/1.0195ec8a.chunk.js"
+  },
+  {
+    "revision": "5622afb9e3b78ee9faa8",
+    "url": "/static/css/main.bca82470.chunk.css"
+  },
+  {
+    "revision": "0195ec8aaa44c3cd8c28",
+    "url": "/static/css/1.d0165e60.chunk.css"
+  },
+  {
+    "revision": "1923bf6c778c122aef768c590a025907",
+    "url": "/index.html"
+  }
+];
